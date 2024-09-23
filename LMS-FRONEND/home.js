e3054f9ed9cd41556f6dad105666f069a5219d07
Ragula Karthik <95897152+Ragulakarthik@users.noreply.github.com>
2024-09-23 21:19:02 +0530

    Delete LMS-FRONEND directory

--- LMS-FRONEND/home.js
+++
@@ -1,9 +0,0 @@
-// General function to handle actions
-function handleAction(event, action) {
-    event.preventDefault();
-    
-    if (action === 'showRegisterForm') {
-        document.getElementById("registerFormSection").style.display = "block";
-        window.location.href = "#registerFormSection";
-    }
-}
